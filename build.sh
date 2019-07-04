e48d3e5354c688a0701ec75a81595a73f217eb66
Jack Byrne <jack.byrne@durham.ac.uk>
2019-07-04 22:55:01 +0100

    build system, gitignore

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "Building Infrared"
+echo "...Cleaning Build Directory"
+rm -rf ./build
+mkdir build
+
+echo "...Creating Build Templates"
+echo "...#!/bin/sh
+kotlin -classpath ./build/infrared.jar CLASS" > ./build/kotlin.template
+
+echo "...Entering Build Directory"
+cd build
+echo "...Compiling Kotlin Source"
+kotlinc ../src -d infrared
+echo "...Compiling Java Source"
+javac $(find ../src/ | rg .java) -d infrared
+echo "...Building Jar"
+cd infrared
+jar cf ../infrared.jar -C "." $(find ./* | rg .class)
+cd ../
+echo "...Creating Run Scripts"
+cat kotlin.template | sed 's/CLASS/io.github.jgbyrne.core.MainKt/' > main; chmod +x main
+echo "       build/main"
+cat kotlin.template | sed 's/CLASS/terrain.TerrainKt/' > terrain; chmod +x terrain
+echo "       build/terrain"
+echo "...Leaving Build Directory"
+cd ..
+echo "Done!"
---
+++ src/terrain/terrain.kt
@@ -0,0 +1,25 @@
+package terrain
+
+import terrain.noise.OpenSimplexNoise
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+fun main() {
+    val WIDTH: Int = 512
+    val HEIGHT: Int = 512
+    val FEATURE_SIZE: Double = 24.0
+
+    var noise = OpenSimplexNoise()
+
+    var img = BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB)
+    for (y in 0 until HEIGHT - 1) {
+        for (x in 0 until WIDTH - 1) {
+            val value = noise.eval(x / FEATURE_SIZE, y / FEATURE_SIZE, 0.0)
+            println(((65793 * ((value + 1) * 127.5)) + (512 * y)).toInt())
+            val rgb = minOf(65793, ((65793 * ((value + 1) * 127.5)) + (512 * y)).toInt())
+            img.setRGB(x, y, rgb)
+        }
+    }
+    ImageIO.write(img, "png",  File("noise.png"))
+}
